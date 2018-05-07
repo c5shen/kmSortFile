538318beeb61ab6c4e2a1b53a2745ef1b47ea476
Chengze Shen <c5shen@ucsd.edu>
2018-05-07 13:13:55 -0700

    finished objs on 5.7.2018

--- kmSortFile.py
+++ kmSortFile.py
@@ -4,47 +4,8 @@ import pandas as pd
 from scipy import stats
 from cuzcatlan import elemental
 
-def kmSortFile(num_clusters, name, P_VALUE=0.005, report_only_diff_expr_genes=1):
-
-
-    # input a number num_clusters to indicate number of clusters assigned
-    # input a name for the basis of cluster files name
-    #num_clusters = int(sys.argv[1])
-    #name = sys.argv[2]
-
-    # original data with combined clusters
-    original = open(name+'.gct')
-
-    clusters = []
-    for i in range(1,num_clusters+1):
-        clusters.append(open(name + '-' + str(i) + '.gct'))
-
-    temp = original.read().split('\n')
-    num_rows = int(temp[1].split('\t')[0])
-    num_columns = int(temp[1].split('\t')[1])
-
-    temp_data = []      # temporarily holds data for future numpy format
-    #description = []    # stores descriptions for each probe
-    #probe_names = []    # stores names of each probe
-
-    # pull down the data to different chunks.
-    # 1) data part -> matrix
-    for i in range(3, len(temp)):
-        if temp[i] != '':
-            processing = temp[i].split('\t')
-            temp_data.append(list(processing[2:]))
-            #description.append(processing[1])
-            #probe_names.append(processing[0])
-            del processing
-    matrix = np.array(temp_data, dtype=float)
-
-    # get clusters info (num of rows, starting location in matrix)
-    clusters_info = []
-    starting_loc = 0
-    for i in range(num_clusters):
-        temp_num = int(clusters[i].read().split('\n')[1].split('\t')[1])
-        clusters_info.append((starting_loc, temp_num))
-        starting_loc += temp_num
+# t test for detecting differentially expressed genes
+def expression_t_test(num_rows, info, data, p):
 
     # use a dictionary to store all uniquely expressed clusters
     # the key is index of probe, and the value is the index of cluster
@@ -52,23 +13,23 @@ def kmSortFile(num_clusters, name, P_VALUE=0.005, report_only_diff_expr_genes=1)
     diff_expression = {}
 
     # Find uniquely expressed/repressed genes of the provided clusters
-    for i in range(num_clusters):
-        start = clusters_info[i][0]
-        length = clusters_info[i][1]
+    for i in range(len(info)):
+        start = info[i][0]
+        length = info[i][1]
 
         # for each gene, flag the current cluster as test, and others as
         # compared calculate the mean for each and do a two-sample t-test to
         # see if the cluster gene expression is uniquely expressed
         for j in range(num_rows):
-            test = np.copy(matrix[j][start:start+length])
-            compared = np.copy(matrix[j])
+            test = np.copy(data[j][start:start+length])
+            compared = np.copy(data[j])
             for item in test:
                 compared = np.delete(compared, item)
             
             # if the expression level is statistically significant (0.05),
             # both tails, then include this in the different expressed clusters
             p_value = stats.ttest_ind(test, compared)[1]
-            if (p_value < P_VALUE):
+            if (p_value < p):
 
                 # if there is already another cluster has the same differently
                 # expressed gene, check the p_value and the smaller one wins
@@ -82,15 +43,46 @@ def kmSortFile(num_clusters, name, P_VALUE=0.005, report_only_diff_expr_genes=1)
                 else:
                     diff_expression[j] = (i, p_value)
 
-    # output to a file named after the original name, with 'sorted' tagged
-    # behind
-#    output = open(name + '-sorted.gct', 'w')
+    return diff_expression
 
-    # write the header and # of rows/columns, also write the names of sample
-#    output.write(header + '\n' + info + '\n')
-#    output.write("Name" + '\t' + "Description" + '\t' + 
-#                      '\t'.join(sample_names) + '\n')
 
+# main function for sorting genes based on differentially expressed level
+def kmSortFile(num_clusters, name, P_VALUE=0.005, report_only_diff_expr_genes=1):
+
+    # original data with combined clusters
+    original = open(name+'.gct')
+
+    clusters = []
+    for i in range(1,num_clusters+1):
+        with open(name+'-'+str(i)+'.gct') as myfile:
+            head = [next(myfile) for x in range(2)]
+            clusters.append(head)
+        #clusters.append(open(name + '-' + str(i) + '.gct'))
+
+    temp = original.read().split('\n')
+    num_rows = int(temp[1].split('\t')[0])
+    num_columns = int(temp[1].split('\t')[1])
+
+    temp_data = []      # temporarily holds data for future numpy format
+
+    # pull down the data to different chunks.
+    # 1) data part -> matrix
+    for i in range(3, len(temp)):
+        if temp[i] != '':
+            processing = temp[i].split('\t')
+            temp_data.append(list(processing[2:]))
+            del processing
+    matrix = np.array(temp_data, dtype=float)
+
+    # get clusters info (starting location and num of items in each cluster)
+    clusters_info = []
+    starting_loc = 0
+    for i in range(num_clusters):
+        temp_num = int(clusters[i][1].split('\t')[1])
+        clusters_info.append((starting_loc, temp_num))
+        starting_loc += temp_num
+
+    diff_expression = expression_t_test(num_rows, clusters_info, matrix, P_VALUE)
 
     # construct a int array containing length of num of rows, start writing in
     # rows that are differently expressed by each cluster one by one,
@@ -98,47 +90,29 @@ def kmSortFile(num_clusters, name, P_VALUE=0.005, report_only_diff_expr_genes=1)
     # IN ORDER to the file
     rest_rows = [x for x in range(num_rows)]
 
-    # iterate through the whole dictionary for each cluster, so we can print
-    # out the corresponding differently expressed genes in cluster order
-#    for k in range(num_clusters):
-#        for key,value in diff_expression.items():
-#            if value[0] == k:
-#                output.write(probe_names[key] + '\t' + description[key] + '\t')
-#                for i in range(len(matrix[key])):
-#                    output.write(str(matrix[key][i]) + '\t')
-#                output.write('\n')
-#                rest_rows.remove(key)   # remove the added ones from rest_rows
-
-#    for i in range(len(rest_rows)):
-#        output.write(probe_names[rest_rows[i]] + '\t' + 
-#                         description[rest_rows[i]] + '\t')
-#        for j in range(len(matrix[rest_rows[i]])):
-#            output.write(str(matrix[rest_rows[i]][j]) + '\t')
-#        output.write('\n')
-        
-#    output.close()
-
     df_to_process = pd.read_table(name+'.gct', skiprows=2)
     new_order = []
+
     # rearrange the rows to pass the matrix for an np ndarray, then construct
     # pandas DataFrame
     for key, value in sorted(diff_expression.items(), key=lambda item :(item[1][0],item[0]) ):
-        #print("%s: %s" % (key,value))
         rest_rows.remove(key)
         new_order.append(key)
-    certainty = len(new_order)
+
+    certainty = len(new_order)  # the ones that we know have certainty of diff expressed
     for item in rest_rows:
         new_order.append(item)
 
-    #print(df_to_process.index.values)
+    # reorder the pandas table by the new order, then change the indexing back
+    # to 'Name' column
     df_to_process = df_to_process.reindex(new_order)
     df_to_process.set_index('Name',inplace=True)
-    #df_to_process.drop('',axis=1)
-    #print(df_to_process.head(certainty))
 
+    # report whole genes or only the differentially expressed ones
     if report_only_diff_expr_genes:
         elemental.df2gct(df_to_process.head(certainty), 1, True, name+'-sorted.gct', False)
     else:
         elemental.df2gct(df_to_process, 1, True, name+'-sorted.gct', False)
 
-kmSortFile(int(sys.argv[1]), sys.argv[2], float(sys.argv[3]), int(sys.argv[4]))
+# test line for individually calling the .py file with command line inputs
+#kmSortFile(int(sys.argv[1]), sys.argv[2], float(sys.argv[3]), int(sys.argv[4]))
